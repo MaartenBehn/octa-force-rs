9d29a3d926e478bf74bf17855c093a71d0bcb531
Maarten Behn <maarten.behn@gmail.com>
2025-05-03 23:02:36 +0200

    descriptorheap

--- src/vulkan/descriptor_heap.rs
+++ src/vulkan/descriptor_heap.rs
@@ -1,24 +1,32 @@
-use std::sync::Arc;
+use std::{cell::RefCell, rc::Rc, sync::Arc, usize};
 use ash::vk;
 
 use crate::OctaResult;
 
-use super::{Context, DescriptorPool, DescriptorSet, DescriptorSetLayout, Device};
+use super::{Context, DescriptorPool, DescriptorSet, DescriptorSetLayout, Device, ImageAndView, ImageView};
 
 #[derive(Debug)]
 pub struct DescriptorHeap {
+    device: Arc<Device>,
+    pub heap_types: Vec<vk::DescriptorPoolSize>,
     pub pool: DescriptorPool,
     pub layout: DescriptorSetLayout,
     pub set: DescriptorSet,
-} 
+    allocator: Rc<RefCell<DescriptorHeapHandleAllocator>>,
+}
+
+pub struct DescriptorHandle {
+    pub value: u32,
+    allocator: Rc<RefCell<DescriptorHeapHandleAllocator>>
+}
 
 impl DescriptorHeap {
-    pub(crate) fn new(device: Arc<Device>, heap_types: &[vk::DescriptorPoolSize]) -> OctaResult<Self> {
+    pub(crate) fn new(device: Arc<Device>, heap_types: Vec<vk::DescriptorPoolSize>) -> OctaResult<Self> {
         
         let pool = DescriptorPool::new(
             device.clone(), 
             1,
-            heap_types,
+            &heap_types,
             vk::DescriptorPoolCreateFlags::UPDATE_AFTER_BIND
         )?;
 
@@ -42,20 +50,117 @@ impl DescriptorHeap {
         let layout = DescriptorSetLayout::new(device.clone(), &bindings, &flags)?;
 
         let set = pool.allocate_set(&layout)?;
-       
+      
+        let allocator = Rc::new(RefCell::new(DescriptorHeapHandleAllocator::new()));
+
         Ok(Self {
+            device,
+            heap_types,
             pool,
             layout,
             set,
+            allocator,
         })
+    }
+
+    pub fn create_image_handle(&mut self, view: &ImageView, usage: vk::ImageUsageFlags) -> OctaResult<DescriptorHandle> {
+        let handle = self.allocator.borrow_mut().alloc();
+
+        let img_info = vk::DescriptorImageInfo::default()
+            .image_view(view.inner);
+
+        let wds = vk::WriteDescriptorSet::default()
+            .dst_set(self.set.inner)
+            .dst_array_element(handle)
+            .descriptor_count(1);
+
+        if usage.contains(vk::ImageUsageFlags::SAMPLED) {
+            let img_info = img_info.image_layout(
+                if usage.contains(vk::ImageUsageFlags::STORAGE) { 
+                    vk::ImageLayout::GENERAL 
+                } else { 
+                    vk::ImageLayout::READ_ONLY_OPTIMAL
+                }
+            );
+
+            let binding_index = self.heap_types.iter()
+                .position(|t| t.ty == vk::DescriptorType::SAMPLED_IMAGE )
+                .ok_or(anyhow::anyhow!("Descriptor heap must have sampled image type!"))?;
+
+            let wds = wds.descriptor_type(vk::DescriptorType::SAMPLED_IMAGE)
+                .dst_binding(binding_index as _)
+                .image_info(std::slice::from_ref(&img_info));
+
+            unsafe { self.device.inner.update_descriptor_sets(&[wds], &[]) };
+        } 
+
+        if usage.contains(vk::ImageUsageFlags::STORAGE) {
+            let img_info = img_info.image_layout(vk::ImageLayout::GENERAL);
+
+            let binding_index = self.heap_types.iter()
+                .position(|t| t.ty == vk::DescriptorType::STORAGE_IMAGE )
+                .ok_or(anyhow::anyhow!("Descriptor heap must have storage image type!"))?;
+
+            let wds = wds.descriptor_type(vk::DescriptorType::STORAGE_IMAGE)
+                .dst_binding(binding_index as _)
+                .image_info(std::slice::from_ref(&img_info));
+            
+            unsafe { self.device.inner.update_descriptor_sets(&[wds], &[]) };
+        } 
+
+        Ok(DescriptorHandle { value: handle, allocator: self.allocator.clone() })
     }
 }
 
 impl Context {
     pub fn create_descriptor_heap(
         &self,
-        heap_types: &[vk::DescriptorPoolSize],
+        heap_types: Vec<vk::DescriptorPoolSize>,
     ) -> OctaResult<DescriptorHeap> {
         DescriptorHeap::new(self.device.clone(), heap_types)
+    }
+}
+
+
+#[derive(Debug)]
+pub(crate) struct DescriptorHeapHandleAllocator {
+    next_free_word_id: usize,
+    used_map: Vec<u64>,
+}
+
+impl DescriptorHeapHandleAllocator {
+
+    pub fn new() -> Self {
+        let capacity = 1024 * 64;
+        
+        Self {
+            next_free_word_id: 0,
+            used_map: vec![0; (capacity + 63) / 64],
+        }
+    }
+
+    pub fn alloc(&mut self) -> u32 {
+        for i in 0..self.used_map.len() {
+            let wi = (i + self.next_free_word_id) % self.used_map.len();
+            if self.used_map[wi] != !0 {
+                let j = self.used_map[wi].count_ones();
+                self.used_map[wi] |= 1 << j;
+                self.next_free_word_id = wi;
+            
+                return wi as u32 * 64 + j;
+            }
+        }
+        return !0;
+    }
+    
+    pub fn free(&mut self, addr: u32) {
+        self.used_map[(addr / 64) as usize] &= !(1 << (addr & 63));
+    }
+}
+
+
+impl Drop for DescriptorHandle {
+    fn drop(&mut self) {
+        self.allocator.borrow_mut().free(self.value);
     }
 }
